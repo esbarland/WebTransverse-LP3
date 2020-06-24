48ffa85dd384fae2a01c524b4c33c8d34db93d0b
thbailly1 <thibaut.bailly@etu.uca.fr>
2020-06-24 19:19:19 +0200

    Add Server with querries

---
+++ Server/src/schema.js
@@ -0,0 +1,42 @@
+import { merge } from 'lodash';
+import { makeExecutableSchema } from 'graphql-tools';
+
+// User typedefs and resolvers
+import {
+  typeDef as User,
+  resolvers as userResolvers,
+} from './schema/User.schema';
+
+import {
+  typeDef as Project,
+  resolvers as projectResolvers,
+} from './schema/Project.schema';
+
+import {
+  typeDef as Task,
+  resolvers as taskResolvers,
+} from './schema/Task.schema';
+// Add more schema and model here
+//....
+//....
+
+// General query
+const Query = `
+  type Query {
+    _empty: String
+  }
+  type Mutation {
+    _empty: String
+  }
+  type Subscription {
+    _empty: String
+  }
+`;
+
+const resolvers = {};
+
+// Do not forget to merge at the end of typeDefs and resolvers
+export const schema = makeExecutableSchema({
+  typeDefs: [ Query, User, Project, Task],
+  resolvers: merge(resolvers, userResolvers, projectResolvers, taskResolvers),
+});
